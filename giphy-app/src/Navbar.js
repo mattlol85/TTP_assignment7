9534f97377d8888f5700811cc46388a27f733194
Matthew Fitzgerald <mattlol85@gmail.com>
2022-01-13 16:35:04 -0500

    Made more component skeletons.

---
+++ giphy-app/src/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './Navbar.css';
+
+export default function Navbar(){
+    return(
+        <div id="navbar">
+            <h1 id="navBarTitle">GIPHY Search</h1>
+        </div>
+    )
+}
