55ef5f6b6048ae5f29a6ec78052e50e84fc7e1e8
Gallelee <thimotus@gmail.com>
2022-01-14 15:38:00 -0500

    Add the random url and button

--- giphy-app/src/App.js
+++ giphy-app/src/App.js
@@ -2,26 +2,39 @@ import './App.css';
 import Footer from './Footer'
 import Navbar from './Navbar';
 import SearchField from './SearchField';
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {My_Api_key} from "./config"
 
 function App() {
   const [searchInput, setSearchInput] = useState(null)
 
   const [gifs, setGifs] = useState(null)
+
+  const [searchForGifs, setSearchForGifs] = useState(false)
+
+  const randomUrl = `http://api.giphy.com/v1/gifs/random?api_key=${My_Api_key.key}`
+
+  useEffect(() => {
+      fetch(randomUrl).then(res => res.json()).then(obj => setGifs(obj))
+    },[randomUrl])
+
   const getInput = (data) =>{
     setSearchInput(data)
   }
-
-
-  console.log(searchInput)
-  return (
-    <div>
-    <Navbar />
-    <SearchField inputFunc={getInput}/>
-    <Footer />
-    </div>
-  );
+  if(gifs) {
+    return (
+        <div>
+          <Navbar/>
+          <SearchField inputFunc={getInput}/>
+          <button>Search</button>
+          <button>Random</button>
+          <Footer/>
+        </div>
+    );
+  }
+  return(
+      <h2>Nothing to see here</h2>
+  )
 }
 
 export default App;
