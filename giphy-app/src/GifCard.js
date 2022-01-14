5914d99bc9670181bcd20bc9a37305c141434c74
Gallelee <thimotus@gmail.com>
2022-01-14 18:24:07 -0500

    Add ability to display gifs based on search or random

--- giphy-app/src/GifCard.js
+++ giphy-app/src/GifCard.js
@@ -6,11 +6,11 @@ export default function GifCard(props){
     const [gifprop, setGitProp] = useState(props.gif)
 
 
-console.log(gifprop)
+console.log(gifprop.data)
     return(
 
         <div>
-            <img src={gifprop.data.images.original.url}/>
+            {Array.isArray(gifprop.data)? gifprop.data.map(elm => <img src={elm.images.original.url}/>) : <img src={gifprop.data.images.original.url}/> }
         </div>
     )
 
